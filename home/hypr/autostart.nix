6c65c9e3261a148e8df23cbef3e53235dd152b7a
e-hucke <dev.ehucke@tuta.com>
2026-05-18 15:24:42 +0200

    2 more fixes

--- home/hypr/autostart.nix
+++ home/hypr/autostart.nix
@@ -5,6 +5,7 @@
   {
     exec-once =
     [
+      "uwsm app -- waybar"
       "uwsm app -- nm-applet --indicator"
       "uwsm app -- wl-paste --type text --watch cliphist store"
       "uwsm app -- wl-paste --type image --watch cliphist store"
--- system/programs.nix
+++ system/programs.nix
@@ -13,6 +13,7 @@
   xdg.portal =
   {
     enable = true;
+    config.common.default = "*";
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     configPackages = with pkgs; [ xdg-desktop-portal-hyprland xdg-desktop-portal-gtk ];
   };
